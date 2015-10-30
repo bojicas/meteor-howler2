b403e0319794fde8358d30ce89a9bd6f8cac3d3d
Silviu D. Bojica <silviu@cubicleapps.com>
2015-10-30 22:34:43 +0400

    Update version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'bojicas:howler2',
-  version: '2.0.0_6',
+  version: '2.0.0_7',
   // Brief, one-line summary of the package.
   summary: 'Meteor package for Howler audio library',
   // URL to the Git repository containing the source code for this package.
